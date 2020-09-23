a835b767b2a94a52535c5da936cae3f0bb52bc30
Brian Ben Maranville <brian.maranville@nist.gov>
2020-09-23 11:04:09 -0400

    adding batch file to replace vbscript

---
+++ extra/create_shortcut.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo dim scriptdir >> CreateShortcut.vbs
+echo scriptdir = CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.ScriptFullName) >> CreateShortcut.vbs
+echo sLinkFile = "%userprofile%\Desktop\Refl1D.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = scriptdir ^&"\pythonw.exe" >> CreateShortcut.vbs
+echo oLink.Arguments = " -m refl1d.main --edit" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = scriptdir >> CreateShortcut.vbs
+echo oLink.Description = "Refl1D" >> CreateShortcut.vbs
+echo oLink.IconLocation = scriptdir ^&"\refl1d.ico" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
