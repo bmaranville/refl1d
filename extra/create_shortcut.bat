42fb882fda82e1703e1dce51a706eda262866bc7
Brian Ben Maranville <brian.maranville@nist.gov>
2020-12-07 14:21:01 -0500

    use directory name from zip file for link name in create_shortcut

--- extra/create_shortcut.bat
+++ extra/create_shortcut.bat
@@ -2,7 +2,9 @@
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo dim scriptdir >> CreateShortcut.vbs
 echo scriptdir = CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.ScriptFullName) >> CreateShortcut.vbs
-echo sLinkFile = "%userprofile%\Desktop\Refl1D.lnk" >> CreateShortcut.vbs
+echo dim dirname >> CreateShortcut.vbs
+echo dirname = CreateObject("Scripting.FileSystemObject").GetFileName(scriptdir) >> CreateShortcut.vbs
+echo sLinkFile = "%userprofile%\Desktop\" ^& dirname ^&".lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = scriptdir ^&"\pythonw.exe" >> CreateShortcut.vbs
 echo oLink.Arguments = " -m refl1d.main --edit" >> CreateShortcut.vbs
