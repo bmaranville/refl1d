c1d0305459bc2399c028a28b7a2406c95577910c
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-05-29 18:24:18 -0400

    use fit worker

--- refl1d/webview/client/src/asyncWorkerSocket.ts
+++ refl1d/webview/client/src/asyncWorkerSocket.ts
@@ -2,9 +2,11 @@ import { wrap, proxy } from 'comlink';
 import type { Endpoint, Remote } from 'comlink';
 // import './standalone_worker';
 import type { Server } from './standalone_worker';
+// import type { Server as FitServer } from './standalone_fit_worker';
 
 export function io() {
   const worker = new Worker(new URL("./standalone_worker.ts", import.meta.url), {type: 'module'});
+  // const fit_worker = new Worker(new URL("./standalone_fit_worker.ts", import.meta.url), {type: 'module'});
   const socket = new AsyncSocket(worker);
   return socket;
 };
@@ -15,15 +17,20 @@ export class AsyncSocket {
   proxy_lookups: Map<EventCallback, typeof proxy<EventCallback>>;
   handlers: { [signal: string]: EventCallback[] }
   ServerPromise: Promise<Remote<Server>>
+  // FitServerPromise: Promise<Remote<FitServer>>
   worker: Worker
+  // fit_worker: Worker
   id: string = "WebWorker"
 
   constructor(worker: Worker) {
     this.handlers = {};
     this.proxy_lookups = new Map();
     this.worker = worker;
+    // this.fit_worker = fit_worker;
     const ServerClass = wrap<Server>(worker);
+    // const FitServerClass = wrap<FitServer>(fit_worker);
     this.ServerPromise = new ServerClass();
+    // this.FitServerPromise = new FitServerClass();
   }
   connect() {}
   on(signal: string, handler: EventCallback) {
@@ -48,6 +55,7 @@ export class AsyncSocket {
     
     const server = await this.ServerPromise;
     await server.mount(dirHandle);
+    await server.asyncEmit('add_notification', {content: dirHandle.name, title: "Local Directory Mounted", timeout: 3000 });
     return true;
   }
   async syncFS() {
--- refl1d/webview/client/src/standalone_worker.ts
+++ refl1d/webview/client/src/standalone_worker.ts
@@ -1,7 +1,9 @@
 // import { loadPyodideAndPackages } from './pyodide_worker.mjs';
-import { expose } from 'comlink';
+import { expose, wrap, proxy } from 'comlink';
 import { loadPyodide, version } from 'pyodide';
 import type { PyodideInterface } from 'pyodide';
+import type { Server as FitServer } from './standalone_fit_worker';
+
 const DEBUG = true;
 
 var pyodide: PyodideInterface;
@@ -25,6 +27,7 @@ async function loadPyodideAndPackages() { // loads pyodide
         "mpld3",
         "periodictable",
         "blinker",
+        "dill",
     ])
     await micropip.install("./wheels/${BUMPS_WHEEL_FILE}")
     await micropip.install("./wheels/${REFL1D_WHEEL_FILE}", keep_going=True, deps=False)
@@ -36,6 +39,8 @@ async function loadPyodideAndPackages() { // loads pyodide
     api.state.problem.serializer = "dataclass"
     print("api imported")
     import refl1d
+    import json
+    import dill
     # setup backend:
     refl1d.use('c_ext')
     await api.emit("add_notification", {
@@ -45,7 +50,8 @@ async function loadPyodideAndPackages() { // loads pyodide
     })
 
     wrapped_api = {}
-    
+    fit_server = object()
+
     def expose(method, method_name):
         def wrapper(args):
             # print("args:", args)
@@ -58,10 +64,53 @@ async function loadPyodideAndPackages() { // loads pyodide
         return wrapper
 
     for method_name, method in api.REGISTRY.items():
+        if method_name == "start_fit_thread":
+            continue
         print("wrapping:", method_name)
         wrapped = expose(method, method_name)
         wrapped_api[method_name] = wrapped
-    
+
+    wrapped_progress = expose(api._fit_progress_handler, "evt_fit_progress")
+    wrapped_complete = expose(api._fit_complete_handler, "evt_fit_complete")
+    wrapped_api["evt_fit_progress"] = wrapped_progress
+    wrapped_api["evt_fit_complete"] = wrapped_complete
+
+    async def start_fit_thread(fitter_id: str="", options=None, terminate_on_finish=False):
+        print("starting fit thread", fitter_id, options)
+        state = api.state
+        fitProblem = state.problem.fitProblem if state.problem is not None else None
+        if fitProblem is None:
+            await api.log("Error: Can't start fit if no problem loaded")
+        else:
+            if state.fit_thread is not None:
+                # warn that fit is alread running...
+                logger.warning("fit already running...")
+                await log("Can't start fit, a fit is already running...")
+                return
+            # TODO: better access to model parameters
+            num_params = len(fitProblem.getp())
+            if num_params == 0:
+                raise ValueError("Problem has no fittable parameters")
+
+            dumped = dill.dumps(fitProblem)
+            await api.emit("set_fit_thread_problem", dumped)
+            num_steps = api.get_num_steps(fitter_id, num_params, options)
+            state.fit_stored_problem = fitProblem
+            fit_thread = fitter_id
+
+            await api.emit("fit_progress", {}) # clear progress
+            state.shared.active_fit = api.to_json_compatible_dict(dict(fitter_id=fitter_id, options=options, num_steps=num_steps))
+            await api.log(json.dumps(api.to_json_compatible_dict(options), indent=2), title = f"starting fitter {fitter_id}")
+            state.autosave()
+            await api.emit("add_notification", {
+                "title": "Fit Started",
+                "content": f"Fit started with problem: {api.state.problem.fitProblem.name}",
+                "timeout": 2000,
+            });
+            await api.emit("start_fit_thread_fit", fitter_id, options, terminate_on_finish)
+
+    wrapped_api["start_fit_thread"] = expose(start_fit_thread, "start_fit_thread")
+
     wrapped_api
     `);
     return api;
@@ -70,7 +119,10 @@ async function loadPyodideAndPackages() { // loads pyodide
 // export { loadPyodideAndPackages };
 
 let pyodideReadyPromise = loadPyodideAndPackages(); // run the functions stored in lines 4
-
+const fit_worker = new Worker(new URL("./standalone_fit_worker.ts", import.meta.url), {type: 'module'});
+const FitServerClass = wrap<FitServer>(fit_worker);
+const FitServerPromise = new FitServerClass();
+const FitSignals = ["progress"]
 type EventCallback = (message?: any) => any;
 
 export class Server {
@@ -85,6 +137,7 @@ export class Server {
 
     async init() {
         const api = await pyodideReadyPromise;
+        const fit_server = await FitServerPromise;
         const defineEmit = await pyodide.runPythonAsync(`
             def defineEmit(server):
                 api.emit = server.asyncEmit;
@@ -92,6 +145,35 @@ export class Server {
             defineEmit
          `);
         await defineEmit(this);
+        const define_fit_server = await pyodide.runPythonAsync(`
+            def define_fit_server(fit_server_js):
+                global fit_server
+                fit_server = fit_server_js
+                            
+            define_fit_server
+        `);
+        await define_fit_server(fit_server);
+        this.addHandler('set_fit_thread_problem', async (problem: any) => {
+            console.log('set_fit_thread_problem:', problem);
+            console.log({fit_server}, fit_server.onAsyncEmit);
+            const result = await fit_server.onAsyncEmit('set_problem', problem);
+            console.log("result:", result);
+        });
+        this.addHandler('start_fit_thread_fit', async (...args: any[]) => {
+            console.log('start_fit_thread_fit:', args);
+            const result = await fit_server.onAsyncEmit('start_fit_thread', ...args);
+            console.log("result:", result);
+        });
+        const fit_progress_handler = async (event: any) => {
+            console.log('evt_fit_progress:', event);
+            await this.onAsyncEmit('evt_fit_progress', event);
+        }
+        fit_server.addHandler('evt_fit_progress', proxy(fit_progress_handler));
+        const fit_complete_handler = async (event: any) => {
+            console.log('evt_fit_complete:', event);
+            await this.onAsyncEmit('evt_fit_complete', event);
+        }
+        fit_server.addHandler('evt_fit_complete', proxy(fit_complete_handler));
     }
 
     async addHandler(signal: string, handler: EventCallback) {
@@ -102,6 +184,7 @@ export class Server {
         }
         if (signal === 'connect') {
             await pyodideReadyPromise;
+            await FitServerPromise;
             await handler();
         }
         this.handlers[signal] = signal_handlers;
@@ -124,22 +207,32 @@ export class Server {
         console.log({dirHandle});   
         const nativefs = await pyodide.mountNativeFS("/home/pyodide/user_mount", dirHandle);
         this.nativefs = nativefs;
+        await pyodide.runPythonAsync(`
+        import os
+        os.chdir("/home/pyodide/user_mount")
+
+        `);
     }
 
     async syncFS() {
         let r = await this.nativefs?.syncfs?.();
     }
 
-    async asyncEmit(signal: string, message?: any) {
-        const jsMessage = message?.toJs?.({dict_converter: Object.fromEntries}) ?? message;
-        console.log('server emit:', signal, jsMessage);
+    async asyncEmit(signal: string, ...args: unknown[]) {
+        // this is for emit() calls from the python server
+        const js_args = args.map((arg) => {
+            return arg?.toJs?.({dict_converter: Object.fromEntries}) ?? arg;
+        });
+        // const jsMessage = message?.toJs?.({dict_converter: Object.fromEntries}) ?? message;
+        console.log('server emit:', signal, js_args);
         const handlers = this.handlers[signal] ?? [];
         for (let handler of handlers) {
-            handler(jsMessage);
+            handler(...js_args);
         }
     }
 
     async onAsyncEmit(signal: string, ...args: any[]) {
+        // this is for emit() calls from the client
         const api = await pyodideReadyPromise;
         const callback = (args[args.length - 1] instanceof Function) ? args.pop() : null;
         const result = await api.get(signal)(args);
